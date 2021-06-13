b766fcbace4de01d4e4da4ec14f0008dae8641c4
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-06-13 16:29:48 +0200

    Testing netlify

--- netlify/functions/weather/test.js
+++ netlify/functions/weather/test.js
@@ -1,11 +1,10 @@
 exports.handler = async function (event, context) {
     const nodeVersion = process.env.NODE_VERSION
     const weatherApi = process.env.GEONAMES_USERNAME
-    const combined = `${nodeVersion}-${weatherApi}`
+    const combined = nodeVersion + '-' + weatherApi
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: combined,
-        }),
+        body: JSON.stringify({ message: 'Hello World' + combined }),
     }
 }
