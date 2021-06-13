c8e220af4de56ce5261d301e82a39bf7659fa191
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-06-13 16:19:01 +0200

    Update response format

--- netlify/functions/weather/test.js
+++ netlify/functions/weather/test.js
@@ -2,5 +2,10 @@ exports.handler = async function (event, context) {
     const nodeVersion = process.env.NODE_VERSION
     const weatherApi = process.env.GEONAMES_USERNAME
     const combined = `${nodeVersion}-${weatherApi}`
-    return combined
+    return {
+        statusCode: 200,
+        body: {
+            message: combined,
+        },
+    }
 }
